0891662e6ceea20c098bb40c47a6b735d83edbe4
Паша <39145638+odinvolk@users.noreply.github.com>
2018-05-19 15:23:10 +0300

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+repo="https://github.com/symbiose/symbiose.git"
+zipball="https://github.com/symbiose/symbiose/archive/master.zip"
+dest="symbiose"
+
+if [ -d "$dest" ] ; then
+	echo "$dest already exists, skipping download"
+else
+	echo "Downloading Symbiose..."
+	if [ type git >/dev/null 2>&1 ] ; then
+		echo "git not found, falling back to zipball"
+		wget "$zipball" -O "$dest.zip"
+		unzip "$dest.zip"
+		mv "symbiose-master" "$dest"
+		rm "$dest.zip"
+	else
+		git clone "$repo" "$dest"
+	fi
+fi
+
+cd "$dest"
+
+httpdUser=`ps -eo user,args | egrep -v "(root|$USER)" | egrep "(apache|httpd|nginx)" | awk 'FNR<2 {print $1}'`
+if [ -z "$httpdUser" ] ; then
+	echo >&2 "Could not determine httpd user!"
+	echo >&2 "If you plan to use Symbiose with httpd, please see https://github.com/symbiose/symbiose/wiki/Installing#copying-files-to-the-server"
+else
+	echo "Detected httpd user: $httpdUser"
+	echo "Allowing httpd user to read/write files..."
+	if [ type setfacl >/dev/null 2>&1 ] ; then
+		echo "setfacl not found, falling back to chown"
+		chown -R $httpdUser .
+	else
+		setfacl -R -m d:u:$httpdUser:rwX,u:$httpdUser:rwX .
+	fi
+fi
+
+if [ type npm >/dev/null 2>&1 ] ; then
+	echo >&2 "npm not found, skipping building"
+else
+	echo "Building..."
+	npm install
+	npm run-script build
+	cd build
+	dest="$dest/build"
+fi
+
+echo ""
+echo "Symbiose successfully installed in $dest"
+echo "To configure it, see https://github.com/symbiose/symbiose/wiki/Installing#configuration"
+
+if [ -x /usr/sbin/nginx ] ; then
+	echo "WARNING: If you're using Nginx, you'll have to update your config: see https://github.com/symbiose/symbiose/blob/master/nginx.conf"
+fi
